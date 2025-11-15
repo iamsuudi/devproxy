f0fc4293cdae2d1b12b604330fa0104d1f9fb3f0
Abdulfetah <suudiabdulfetah@gmail.com>
2025-11-15 14:35:28 +0300

    feat: add project directory structure and path management

--- script.sh
+++ script.sh
@@ -23,3 +23,19 @@ case "$CMD" in
         echo_color "$CYAN" "Usage: devproxy <init|start|stop|regen|status>"
         ;;
 esac
+
+# ----------------------------
+# Paths
+# ----------------------------
+PROJECT_DIR=$(pwd)
+PROJECT_NAME=$(basename "$PROJECT_DIR")
+DEVPROXY_DIR="$PROJECT_DIR/.devproxy"
+IP_FILE="$DEVPROXY_DIR/ip"
+PORT_FILE="$DEVPROXY_DIR/port"
+SSL_CERT="$DEVPROXY_DIR/server.crt"
+SSL_KEY="$DEVPROXY_DIR/server.key"
+NGINX_CONF="$DEVPROXY_DIR/nginx.conf"
+DOCKERFILE="$DEVPROXY_DIR/Dockerfile"
+DOCKER_CONTAINER="${PROJECT_NAME}-nginx"
+
+mkdir -p "$DEVPROXY_DIR"
